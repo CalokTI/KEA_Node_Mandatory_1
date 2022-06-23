3768e04253e174b0828a11b2651a9c5181053323
CalokTI <72436270+CalokTI@users.noreply.github.com>
2022-06-23 20:40:54 +0200

    added notes to git

--- public/git/git.js
+++ public/git/git.js
@@ -1,10 +1,10 @@
 const dataArray = [
     "Click",
-    "Click",
-    "Click",
-    "Click",
-    "Click",
-    "Click"
+    "git add 'file' or -A for all",
+    "git commit -m \"message\"",
+    "git push | uploads your committed data to previously set repository or --set-upstream <remote> <name-of-your-branch>",
+    "git pull | combination of git fetch and merge, pulls from repository and merges with current data",
+    "git clone | clones repository"
 ]
 
 
